39bbbc439f9c287d346ebc006d43e8aa86773d61
Alken0 <54180229+Alken0@users.noreply.github.com>
2023-05-23 14:26:45 +0200

    refactor plotting

--- src/plot.py
+++ src/plot.py
@@ -6,45 +6,65 @@ import numpy as np
 from src import lib
 
 
-# TODO animate with plt.ion() & plt.pause(1)
+class Plotter:
+    def __init__(self, continuous=False, timeout=1, vmin=None, vmax=None):
+        self._continuous = continuous
+        if continuous:
+            plt.ion()
+        self._timeout = timeout
+        self._vmin = vmin
+        self._vmax = vmax
+
+    def set_continuous(self) -> None:
+        self._continuous = True
+        plt.ion()
+
+    def _show(self) -> None:
+        if self._continuous:
+            plt.pause(self._timeout)
+            plt.clf()
+        else:
+            plt.show()
+
+    def density(self, F: np.array, step: Optional[int] = None, figure=1) -> None:
+        """
+        plots the density function of F
+        :param F: Probability Density Function of shape (c, x, y)
+        :param step: shows step in title
+        :param figure: which figure pyplot should use
+        """
+        plt.figure(figure)
+        plt.title(f'Density Function' if step is None else f'Density Function @{step}')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        data = np.swapaxes(lib.density(F), 0, 1)
+        plt.imshow(data, cmap='magma', vmin=self._vmin, vmax=self._vmax)
+        cbar = plt.colorbar()
+        cbar.set_label("density", labelpad=+1)
+        self._show()
+
+    def velocity(self, F: np.array, figure: Optional[int] = 2, step: Optional[int] = None, sidebar=False) -> None:
+        """
+        plots the velocity function of F
+        :param sidebar: if True: renders sidebar -> only do once!
+        :param F: Probability Density Function of shape (c, x, y)
+        :param step: shows step in title
+        :param figure: which figure pyplot should use
+        """
+        plt.figure(figure)
+        plt.title(f'Velocity Function' if step is None else f'Velocity Function @{step}')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        data = lib.velocity(F)
+        data = np.sum(data, axis=0)
+        plt.imshow(data, cmap='magma')
+        if sidebar:
+            cbar = plt.colorbar()
+            cbar.set_label("velocity", labelpad=+1)
+        self._show()
 
 
-def density(F: np.array, step: Optional[int] = None) -> None:
-    """
-    plots the density function of F
-    :param F: Probability Density Function of shape (c, x, y)
-    :param step: shows step in title
-    """
-    plt.figure(1)
-    plt.title(f'Density Function' if step is None else f'Density Function @{step}')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    data = lib.density(F)
-    plt.imshow(data, cmap='magma')
-    cbar = plt.colorbar()
-    cbar.set_label("density", labelpad=+1)
-    plt.show()
-
-
-def velocity(F: np.array, step: Optional[int] = None) -> None:
-    """
-    plots the density function of F
-    :param F: Probability Density Function of shape (c, x, y)
-    :param step: shows step in title
-    """
-    plt.figure(2)
-    plt.title(f'Velocity Function' if step is None else f'Velocity Function @{step}')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    data = lib.velocity(F)
-    data = np.sum(data, axis=0)
-    plt.imshow(data, cmap='magma')
-    cbar = plt.colorbar()
-    cbar.set_label("velocity", labelpad=+1)
-    plt.show()
-
-
-def print_pdf(F):
+def print_pdf(F: np.array):
     """
     prints the probability density function (F) to the terminal
     :param F: Probability Density Function of shape (c, x, y)
@@ -58,7 +78,7 @@ def print_pdf(F):
     print()
 
 
-def print_density_function(F):
+def print_density(F: np.array):
     """
     prints the density of the probability density function (F) to the terminal
     :param F: Probability Density Function of shape (c, x, y)
